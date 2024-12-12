7c2d7836a9d602e852e31eb93b733ec72ee430ea
iiiidaaa <iida_kazunori@cyberagent.co.jp>
2024-12-12 13:30:07 +0900

    fix create gcp client

--- pkg/gcp/client.go
+++ pkg/gcp/client.go
@@ -31,6 +31,12 @@ type GcpClient struct {
 }
 
 func NewGcpClient(ctx context.Context, credentialPath string, l logging.Logger) (GcpServiceClient, error) {
+	if _, err := os.Stat(credentialPath); os.IsNotExist(err) {
+		return nil, nil
+	} else if err != nil {
+		return nil, fmt.Errorf("failed to check google credential file: %w", err)
+	}
+
 	crm, err := cloudresourcemanager.NewService(ctx, option.WithCredentialsFile(credentialPath))
 	if err != nil {
 		return nil, fmt.Errorf("failed to create new Cloud Resource Manager service: err=%w", err)
