aac3d05bca051a6b0ceadd2ab1258bc801570424
han-ekiken <han_ekiken@cyberagent.co.jp>
2025-11-10 19:40:04 +0900

    [add]mockにListRepositoryメソッド追加

--- proto/code/mocks/CodeServiceClient.go
+++ proto/code/mocks/CodeServiceClient.go
@@ -500,6 +500,43 @@ func (_m *CodeServiceClient) ListGitleaksCache(ctx context.Context, in *code.Lis
 	return r0, r1
 }
 
+// ListRepository provides a mock function with given fields: ctx, in, opts
+func (_m *CodeServiceClient) ListRepository(ctx context.Context, in *code.ListRepositoryRequest, opts ...grpc.CallOption) (*code.ListRepositoryResponse, error) {
+	_va := make([]interface{}, len(opts))
+	for _i := range opts {
+		_va[_i] = opts[_i]
+	}
+	var _ca []interface{}
+	_ca = append(_ca, ctx, in)
+	_ca = append(_ca, _va...)
+	ret := _m.Called(_ca...)
+
+	if len(ret) == 0 {
+		panic("no return value specified for ListRepository")
+	}
+
+	var r0 *code.ListRepositoryResponse
+	var r1 error
+	if rf, ok := ret.Get(0).(func(context.Context, *code.ListRepositoryRequest, ...grpc.CallOption) (*code.ListRepositoryResponse, error)); ok {
+		return rf(ctx, in, opts...)
+	}
+	if rf, ok := ret.Get(0).(func(context.Context, *code.ListRepositoryRequest, ...grpc.CallOption) *code.ListRepositoryResponse); ok {
+		r0 = rf(ctx, in, opts...)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*code.ListRepositoryResponse)
+		}
+	}
+
+	if rf, ok := ret.Get(1).(func(context.Context, *code.ListRepositoryRequest, ...grpc.CallOption) error); ok {
+		r1 = rf(ctx, in, opts...)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // PutCodeScanRepository provides a mock function with given fields: ctx, in, opts
 func (_m *CodeServiceClient) PutCodeScanRepository(ctx context.Context, in *code.PutCodeScanRepositoryRequest, opts ...grpc.CallOption) (*emptypb.Empty, error) {
 	_va := make([]interface{}, len(opts))
--- proto/code/mocks/CodeServiceServer.go
+++ proto/code/mocks/CodeServiceServer.go
@@ -407,6 +407,36 @@ func (_m *CodeServiceServer) ListGitleaksCache(_a0 context.Context, _a1 *code.Li
 	return r0, r1
 }
 
+// ListRepository provides a mock function with given fields: _a0, _a1
+func (_m *CodeServiceServer) ListRepository(_a0 context.Context, _a1 *code.ListRepositoryRequest) (*code.ListRepositoryResponse, error) {
+	ret := _m.Called(_a0, _a1)
+
+	if len(ret) == 0 {
+		panic("no return value specified for ListRepository")
+	}
+
+	var r0 *code.ListRepositoryResponse
+	var r1 error
+	if rf, ok := ret.Get(0).(func(context.Context, *code.ListRepositoryRequest) (*code.ListRepositoryResponse, error)); ok {
+		return rf(_a0, _a1)
+	}
+	if rf, ok := ret.Get(0).(func(context.Context, *code.ListRepositoryRequest) *code.ListRepositoryResponse); ok {
+		r0 = rf(_a0, _a1)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*code.ListRepositoryResponse)
+		}
+	}
+
+	if rf, ok := ret.Get(1).(func(context.Context, *code.ListRepositoryRequest) error); ok {
+		r1 = rf(_a0, _a1)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // PutCodeScanRepository provides a mock function with given fields: _a0, _a1
 func (_m *CodeServiceServer) PutCodeScanRepository(_a0 context.Context, _a1 *code.PutCodeScanRepositoryRequest) (*emptypb.Empty, error) {
 	ret := _m.Called(_a0, _a1)
